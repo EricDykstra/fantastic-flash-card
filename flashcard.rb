aaef41a272b734c2ac18a9cf4313f7d208460d5c
Apprentice <apprentice@dbc32.local>
2013-03-22 16:21:21 -0700

    Second Commit (T&C)

--- flashcard.rb
+++ flashcard.rb
@@ -1,1 +1,27 @@
 # Tom and Carolyn Flashcards
+class Card
+  def initialize(question, answer)
+    @question = question
+    @answer = answer
+  end 
+end
+
+
+class Deck
+
+  def initialize
+    @cards = [] #our text file will go here
+    @current = nil
+  end
+
+  def draw_card
+    @current = @cards.pop
+  end
+
+  def shuffle
+    @cards.shuffle
+  end
+
+end
+
+
